40a4cea1eb919eac3d87376e2e290f2eb62d676a
junaid sultan <jsultanwork@gmail.com>
2026-02-21 09:55:47 +0500

    page related custom exceptions added

---
+++ src/main/java/com/junaid/simpleswing/exceptions/IncompatiblePageException.java
@@ -0,0 +1,7 @@
+package com.junaid.simpleswing.exceptions;
+
+public class IncompatiblePageException extends RuntimeException {
+    public IncompatiblePageException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/junaid/simpleswing/exceptions/PageNotFoundException.java
@@ -0,0 +1,7 @@
+package com.junaid.simpleswing.exceptions;
+
+public class PageNotFoundException extends RuntimeException {
+    public PageNotFoundException(String message) {
+        super(message);
+    }
+}
