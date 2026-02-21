1374d553d58c6e0286743e81c28767c8e89ef290
junaid sultan <jsultanwork@gmail.com>
2026-02-21 09:55:26 +0500

    custom swing page added

---
+++ src/main/java/com/junaid/simpleswing/core/SimplePage.java
@@ -0,0 +1,14 @@
+package com.junaid.simpleswing.core;
+
+import javax.swing.*;
+
+public abstract class SimplePage extends JPanel {
+    public abstract void render();
+
+    public void OnInit() {
+        IO.println("OnInit() called on page " + getClass().getSimpleName());
+    }
+    public void OnExit() {
+        IO.println("OnExit() called on page " + getClass().getSimpleName());
+    }
+}
