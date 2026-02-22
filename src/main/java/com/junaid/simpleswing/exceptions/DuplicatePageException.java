ad36a2330927bc5c8a8419745606fab571d872d7
junaidsultanxyz <jsultanwork@gmail.com>
2026-02-23 01:34:10 +0500

    Custom exception if duplicate titles for pages are found

---
+++ src/main/java/com/junaid/simpleswing/exceptions/DuplicatePageException.java
@@ -0,0 +1,7 @@
+package com.junaid.simpleswing.exceptions;
+
+public class DuplicatePageException extends RuntimeException {
+    public DuplicatePageException(String message) {
+        super(message);
+    }
+}
